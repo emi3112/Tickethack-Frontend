e1534336c10f4df2df7f5fab4489f88fab5a9392
Emilie <em_bntd3112@yahoo.com>
2023-04-12 16:52:30 +0200

    update script/cart.html

--- cart.js
+++ cart.js
@@ -1,42 +1,20 @@
-let priceCount = document.querySelectorAll(".price").value;
+// let priceCount = document.querySelectorAll(".price").value;
 
-function deleteCart() {
-	for (let i = 0; i < document.querySelectorAll('.deleteCart').length; i++) {
-		document.querySelectorAll('.deleteCart')[i].addEventListener('click', function () {
-			fetch(`http://localhost:3000/carts/deleteCart/${this.id}`, { method: 'DELETE' })
-				.then(response => response.json())
-				.then(data => {
-					if (data.result) {
-						this.parentNode.remove();
-					}
-				});
-		});
-	}
-}
-
-for (let i = 0; i < document.querySelectorAll('.addTrip').length; i++) {
-    document.querySelectorAll('.addTrip')[i].addEventListener('click',
-      function () {
-        console.log(this);
-        const date = document.querySelector('.hour').value;
-    window.location.assign('cart.html')
+// function deleteCart() {
+// 	for (let i = 0; i < document.querySelectorAll('.deleteCart').length; i++) {
+// 		document.querySelectorAll('.deleteCart')[i].addEventListener('click', function () {
+// 			fetch(`http://localhost:3000/carts/deleteCart/${this.id}`, { method: 'DELETE' })
+// 				.then(response => response.json())
+// 				.then(data => {
+// 					if (data.result) {
+// 						this.parentNode.remove();
+// 					}
+// 				});
+// 		});
+// 	}
+// }
 
 
-	fetch('http://localhost:3000/carts', {
-		method: 'POST',
-		headers: { 'Content-Type': 'application/json' },
-		body: JSON.stringify({date}),
-	}).then(response => response.json())
-		.then(data => {
-				document.querySelector('#cartContainer').innerHTML += `
-                <span class="cities">${data.carts.departure} > ${data.carts.arrival}</span>
-                <span class="hour"> ${data.carts.date}</span>
-                <span class="price">${data.carts.price}$</span>
-                <button class="deleteCart" id='${data.carts.date}'>✖</button>`;
-            deleteCart();
-		});
-      }
-    );
-   }
 
-	
+
+
--- script.js
+++ script.js
@@ -1,3 +1,46 @@
+function buttonCart() {
+    let buttons = document.querySelectorAll('.addTrip')
+    for(let i = 0; i < buttons.length; i ++) {
+        buttons[i].addEventListener('click', function() {
+            console.log(('CLICK oKKKKKK BUTONNNNNNNN'));
+            let price = Number(buttons[i].previousElementSibling.previousElementSibling.textContent)
+            console.log('PRICE ===>', price);
+            let hour = buttons[i].previousElementSibling.previousElementSibling.previousElementSibling.textContent
+            console.log('HOUR ===>', hour);
+
+            let arrival = buttons[i].previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.textContent
+            console.log('ARRIVAL ===>', arrival);
+
+            let departure = buttons[i].previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.textContent
+            console.log('DEPARTURE ===>', departure);
+
+
+            fetch('http://localhost:3000/carts/newCart', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({departure: departure, arrival: arrival, date: hour, price: price}),
+            }).then(response => response.json())
+                .then(data => {
+                    console.log(data);
+                        // document.querySelector('#cartContainer').innerHTML += `
+                        // <span class="cities">${data.carts.departure} > ${data.carts.arrival}</span>
+                        // <span class="hour"> ${data.carts.date}</span>
+                        // <span class="price">${data.carts.price}$</span>
+                        // <button class="deleteCart" id='${data.carts.date}'>✖</button>`;
+                    // deleteCart();
+                    window.location.assign('cart.html')
+                });
+
+                
+                // declarer dep arr date price 
+                
+            })
+    }
+}
+
+
+
+
 function showTrips(data) {
     if(data.trips.length) {
     
@@ -14,13 +57,15 @@ function showTrips(data) {
     
             document.querySelector('#resultContainer').innerHTML += `
             <div class="result">
-        <span class="cities">${trip.departure} > ${trip.arrival}</span>
+        <span class="cities">${trip.departure}</span> <span> > </span> <span>${trip.arrival}</span>
         <span class="hour"> ${date}</span>
-        <span class="price">${trip.price}$</span>
+        <span class="price">${trip.price}</span>  <span> $ </span>
         <span class="addTrip" id="${trip.date}">Book</span>
     </div>`
             
         }
+
+        // buttonCart()
 
         // fonction change page boutton
     } else {
@@ -36,51 +81,49 @@ function showTrips(data) {
 }
 
 
-// function buttonCart() {
-//     let buttons = document.querySelectorAll('.addTrip')
-//     for(let i = 0; i < buttons.length; i ++) {
-//         buttons[i].addEventListener('click' function() {
 
-//         })
-//     }
-// }
+function tripHome() {
 
-
-document.querySelector('#searchTrip').addEventListener('click', function() {
-
-	const departure = document.querySelector('#departure').value
-    const arrival = document.querySelector('#arrival').value
-	const dateReq = document.querySelector('#date').value
-    const newDate = new Date(dateReq)
-    const date = newDate.toLocaleDateString()
-
-    console.log('click OK !!');
-
-    fetch('http://localhost:3000/trips/searchTrips', {
-        method: 'POST',
-		headers: { 'Content-Type': 'application/json' },
-		body: JSON.stringify({departure: departure, arrival: arrival, date: date}),
+    document.querySelector('#searchTrip').addEventListener('click', function() {
+    
+        const departure = document.querySelector('#departure').value
+        const arrival = document.querySelector('#arrival').value
+        const dateReq = document.querySelector('#date').value
+        const newDate = new Date(dateReq)
+        const date = newDate.toLocaleDateString()
+    
+        console.log('click OK !!');
+    
+        fetch('http://localhost:3000/trips/searchTrips', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({departure: departure, arrival: arrival, date: date}),
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data.trips);
+    
+            if(document.querySelector('#tripNotFound') && document.querySelector('img')){
+    
+                console.log('PREMIER IF');
+                
+                document.querySelector('#tripNotFound').remove()
+                document.querySelector('img').remove()
+                showTrips(data)
+                buttonCart()
+    
+            } else {
+    
+                console.log('PREMIER ELSE');
+                document.querySelector('#list').innerHTML = `<div id="resultContainer"></div>`
+                showTrips(data)
+                buttonCart()
+    
+            }
+        })
     })
-    .then(response => response.json())
-    .then(data => {
-        console.log(data.trips);
+}
 
-        if(document.querySelector('#tripNotFound') && document.querySelector('img')){
-
-            console.log('PREMIER IF');
-            
-            document.querySelector('#tripNotFound').remove()
-            document.querySelector('img').remove()
-            showTrips(data)
-
-        } else {
-
-            console.log('PREMIER ELSE');
-            document.querySelector('#list').innerHTML = `<div id="resultContainer"></div>`
-            showTrips(data)
-
-        }
-    })
-})
+tripHome()
 
 
