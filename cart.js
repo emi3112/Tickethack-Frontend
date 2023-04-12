0eef16659321bfdc3c0ec46b42aaec9bc0ac4bc5
Emilie <em_bntd3112@yahoo.com>
2023-04-12 17:17:35 +0200

    carts.js et cart.html modif pour affich trajets

--- cart.js
+++ cart.js
@@ -18,3 +18,37 @@
 
 
 
+function getCarts() {
+	fetch('http://localhost:3000/carts/showCarts').then(response => response.json()).then(data => {
+		if(data.carts) {
+			console.log(data.carts);
+			document.querySelector('.p0').remove()
+			document.querySelector('.p1').remove()
+			for(let cart of data.carts) {
+				document.querySelector('#pageContainer').innerHTML += `
+				<div id="myCart">
+				    <h1>
+			   	         My cart
+				    </h1>
+				        <div class="cartContainer">
+				  <span class="cities">${cart.departure} > ${cart.departure}</span>
+					<span class="hour"> ${cart.departure}</span>
+					<span class="price">${cart.departure}</span>  <span> $ </span>
+					<button class="deleteCart">✖</button>
+				</div>
+				<div id="cost">
+				  <div id="total">
+					<span>Total</span>
+					<span id="count"> 103 </span>  <span> $ </span>
+			    </div>
+				</div>
+			  	    <a href="booking.html">Purchase</a>
+				</div>
+			    </div> 
+		    `
+			}
+		}
+	})
+}
+
+getCarts()
