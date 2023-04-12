f158a06cc35f2e2ca8ab0b31ee7fe0e7b4e46f67
Emilie <em_bntd3112@yahoo.com>
2023-04-12 12:35:51 +0200

    new script

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+function showTrips(data) {
+    if(data.trips.length) {
+    
+        console.log('IF FONCTION', data.trips);
+    
+        for(let trip of data.trips) {
+    
+            console.log('FOR FONCTION', trip);
+    
+            document.querySelector('#resultContainer').innerHTML += `
+            <div class="result">
+        <span class="cities">${trip.departure} > ${trip.arrival}</span>
+        <span class="hour"> ${trip.date}</span>
+        <span class="price">${trip.price}$</span>
+        <span class="addTrip">Book</span>
+    </div>`
+            
+        }
+    } else {
+
+        console.log('ELSE FONCTION');
+        document.querySelector('#list').innerHTML = `
+        <div id="list" class="card">
+        <img src="./images/notfound.png" />
+        <p id="tripNotFound">Trip not found</p>
+        <div id="resultContainer"></div>
+        </div>`
+    }
+}
+
+
 document.querySelector('#searchTrip').addEventListener('click', function() {
 
     // document.querySelectorAll('#result').remove()
@@ -5,16 +36,8 @@ document.querySelector('#searchTrip').addEventListener('click', function() {
 	const departure = document.querySelector('#departure').value
     const arrival = document.querySelector('#arrival').value
 	const dateReq = document.querySelector('#date').value
-
-    console.log('TYPE OF DATE VALUE', typeof(dateReq));
-
     const newDate = new Date(dateReq)
-
     const date = newDate.toLocaleDateString()
-
-    console.log(departure);
-    console.log(arrival);
-    console.log(date);
 
     console.log('click OK !!');
 
@@ -26,28 +49,23 @@ document.querySelector('#searchTrip').addEventListener('click', function() {
     .then(response => response.json())
     .then(data => {
         console.log(data.trips);
-        if(data.trips.length) {
 
-            console.log('IF', data.trips);
+        if(document.querySelector('#tripNotFound') && document.querySelector('img')){
 
+            console.log('PREMIER IF');
+            
             document.querySelector('#tripNotFound').remove()
             document.querySelector('img').remove()
+            showTrips(data)
 
-            for(let trip of data.trips) {
-
-                console.log('FOR', trip);
-
-                document.querySelector('#resultContainer').innerHTML += `
-                <div class="result">
-            <span class="cities">${trip.departure} > ${trip.arrival}</span>
-            <span class="hour"> ${trip.date}</span>
-            <span class="price">${trip.price}$</span>
-            <span><a href="cart.html">Book</a></span>
-        </div>`
-                
-            }
         } else {
-            document.querySelector('#tripNotFound').textContent = 'Trip not found'
+
+            console.log('PREMIER ELSE');
+            document.querySelector('#list').innerHTML = `<div id="resultContainer"></div>`
+            showTrips(data)
+
         }
     })
 })
+
+
