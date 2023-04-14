5a108fd2c215a595e3b81b1869c595bdb061c2e1
Magali <magalinegregim@gmail.com>
2023-04-14 16:13:34 +0200

    create basket function add

--- cart.js
+++ cart.js
@@ -1,63 +1,68 @@
 function deleteCart() {
-	for (let i = 0; i < document.querySelectorAll('.deleteCart').length; i++) {
-		document.querySelectorAll('.deleteCart')[i].addEventListener('click', function () {
-			fetch(`http://localhost:3000/carts/deleteCart/${this.id}`, { method: 'DELETE' })
-				.then(response => response.json())
-				.then(data => {
-
-					if (data.result) {
-						this.parentNode.remove();
-					}
-				});
-		});
-	}
+  for (let i = 0; i < document.querySelectorAll(".deleteCart").length; i++) {
+    document
+      .querySelectorAll(".deleteCart")
+      [i].addEventListener("click", function () {
+        fetch(`http://localhost:3000/carts/deleteCart/${this.id}`, {
+          method: "DELETE",
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            if (data.result) {
+              this.parentNode.remove();
+			  return document.querySelector('#count').textContent = basketValue -= Number(basketValue[i].previousElementSibling.previousElementSibling.textContent)
+            }
+          });
+      });
+  }
 }
 
+fetch("http://localhost:3000/carts/showCarts")
+  .then((response) => response.json())
+  .then((data) => {
+    if (data.carts) {
+      //console.log(data.carts);
 
-fetch('http://localhost:3000/carts/showCarts').then(response => response.json()).then(data => {
-	if(data.carts) {
-		console.log(data.carts);
+      document.querySelector(".p0").remove();
+      document.querySelector(".p1").remove();
 
-		document.querySelector('.p0').remove()
-		document.querySelector('.p1').remove()
-
-		document.querySelector('#pageContainer').innerHTML = `
+      document.querySelector("#pageContainer").innerHTML = `
 		<div id="myCart">
-			<h1>
-				My cart
-			</h1>
-
+			<h1>My cart</h1>
 			<div id="cart"></div>
-
 			<div id="cost">
 			    <div id="total">
 					<span>Total</span>
-					<span id="count"> 103 </span>  <span> $ </span>
+					<span id="count"> </span> <span> $ </span>
 			</div>
 			    <a href="booking.html">Purchase</a>
 			</div>
 			</div>
-		</div> `
+		</div> `;
 
-
-		for(let cart of data.carts) {
-			document.querySelector('#cart').innerHTML += `
+      for (let cart of data.carts) {
+        document.querySelector("#cart").innerHTML += `
 			<div class="cartContainer">
                 <span class="cities">${cart.departure} > ${cart.arrival}</span>
                 <span class="hour"> ${cart.date}</span>
                 <span class="price">${cart.price}</span>  <span> $ </span>
                 <button class="deleteCart" id="${cart.date}" >✖</button>
-            </div> `
-		}
-		deleteCart()
-	}
-})
+            </div> `;
+      }
+      deleteCart();
+	  cartBasket()
+	    }
+  });
 
 
+function cartBasket(){
+	const basket = document.querySelectorAll('.price');
+	let basketValue = 0
 
+  for (let i = 0; i < basket.length; i++) {
+  basketValue += parseFloat(basket[i].textContent)
+  }document.querySelector('#count').textContent = basketValue;
 
-
-
-
+}
 
 
